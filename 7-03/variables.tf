6c39e6f629da1c122d9398b669b099be4ed4961a
StarcevDA <isava2010@gmail.com>
2026-04-05 10:29:40 +0000

    отредактированые файлы cloud-init,variables

--- 7-03/variables.tf
+++ 7-03/variables.tf
@@ -5,11 +5,11 @@ variable "flow" {
 
 variable "cloud_id" {
   type    = string
-  default = "b1gn3ndpua1j6jaabf79"
+  default = "b1ginsvjcfrbhhgqdinb"
 }
 variable "folder_id" {
   type    = string
-  default = "b1gfu61oc15cb99nqmfe"
+  default = "b1gcqsngjoh8cnfo5kbt"
 }
 
 variable "test" {
